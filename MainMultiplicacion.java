46e12fb768b6bf6ba14b56dcc28b9d648d6cd990
Jason Rojas <jasonrojasyy@gmail.com>
2024-05-06 12:26:32 -0600

    version2

---
+++ MainMultiplicacion.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MainMultiplicacion{ 
+    public static void main(String[] args){
+        Scanner w = new Scanner(System.in);
+        int y,x;
+
+         System.out.println("ingresa un numero entero");
+
+         x=op.nextInt();
+
+         System.out.println("ingresa otro numero entero");
+
+         y=op.nextInt();
+
+         Multiplicacion evaluar = new Multiplicacion();
+         evaluar.setX(x);
+         evaluar.setY(y);
+           System.out.println(evaluar.mostrarResultado());
+}
+
+}
